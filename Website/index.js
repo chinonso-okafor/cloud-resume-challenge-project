6f9a79bc303f4b7fba4159c7f46262ef158584eb
nakachukwu.github.io <nonso.o@icloud.com>
2023-07-29 09:58:55 -0600

    index.js

--- Website/index.js
+++ Website/index.js
@@ -4,7 +4,7 @@ async function updateCounter() {
     "https://xohrvm5xsp2xh6dz7hfd5m2sqq0dpuhv.lambda-url.us-east-1.on.aws/"
   );
   let data = await response.json();
-  counter.innerHTML = ` This page has ${data} Views!!`;
+  counter.innerHTML = ` This page has ${data} Views!`;
 }
 
 updateCounter();
